f8a9f35194c25c7531a16bf011667d8582f83114
Andrew Buskov <andrew.buskov@gmail.com>
2026-04-13 10:31:17 -0500

    Testing

--- init-config.sh
+++ init-config.sh
@@ -7,7 +7,7 @@ echo "Initializing config..."
 # If /config is empty, copy everything
 if [ -z "$(ls -A /config 2>/dev/null)" ]; then
     echo "Config is empty, seeding from defaults..."
-    cp -r /defaults/* /config/
+    cp -r /defaults/. /config/
 else
     echo "Config exists, merging updates..."
     rsync -av --update /defaults/data/User/ /config/data/User/
